b795371350dc9e2357d5dfd8b2a518dec23d4a6d
Ash Gao <gaoye0718@gmail.com>
2019-09-07 12:07:55 -0400

    Update libs.py

--- libs.py
+++ libs.py
@@ -1,69 +1,68 @@
 import random
-import time
-import datetime
-import numpy as np
-from numpy import array
-import pandas as pd
-from pydub import AudioSegment
 import os
 import shutil
 import glob
 import gc
 import sys
 import h5py
-from sklearn import model_selection
-from sklearn.ensemble import RandomForestClassifier
+import time
+import datetime
+import pickle
+import librosa
+import warnings
+import matplotlib.pyplot as plt
+
+import numpy as np
+from numpy import array
+import pandas as pd
+from pandas.plotting import parallel_coordinates
+from pydub import AudioSegment
 
 #imported for testing
 import wave
 import contextlib
 
 # for outputing file
+from scipy.cluster.vq import vq, kmeans, whiten
 import scipy.stats.stats as st
 
-from sklearn.datasets import make_classification
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.preprocessing import normalize
-from sklearn.neighbors import KNeighborsClassifier
-from scipy.cluster.vq import vq, kmeans, whiten
 from sklearn.cluster import KMeans
-from sklearn.svm import SVC
-from sklearn.metrics import f1_score
-from sklearn import mixture
-from sklearn.model_selection import train_test_split
-from sklearn.utils import class_weight
-from sklearn.impute import SimpleImputer
-import pickle
-import librosa
-import matplotlib.pyplot as plt
+from sklearn.datasets import make_classification
+from sklearn.datasets.samples_generator import make_blobs
 from sklearn.decomposition import PCA as sklearnPCA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
-from sklearn.datasets.samples_generator import make_blobs
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import normalize
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import auc, confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
-
-from pandas.plotting import parallel_coordinates
-import pickle
+from sklearn import mixture
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.utils import class_weight
 
 import keras
-from keras import optimizers, regularizers
-from keras.layers import Convolution1D, Dense, MaxPooling1D, Flatten
+
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 from keras.constraints import maxnorm
-from keras.layers import Add
+from keras.layers import Convolution1D, Dense, MaxPooling1D, Flatten, Add, Dropout, Input, Activation
+from keras.layers import TimeDistributed, Bidirectional, LSTM
 from keras.models import Sequential
-from keras.layers import TimeDistributed
-from keras.layers import Bidirectional
-from keras.layers import LSTM
-from keras.callbacks import ModelCheckpoint
-from keras.layers import Dropout, Input
-from keras.callbacks import EarlyStopping
-from keras.layers import Activation
-from keras.utils import np_utils
-from keras.utils import to_categorical
+from keras import optimizers, regularizers
+from keras.utils import np_utils, to_categorical
 
 from colorama import Fore, Back, Style
 
 from IPython.display import clear_output
 
-import warnings
-warnings.filterwarnings('ignore')
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+#warnings.filterwarnings('ignore')
+#os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+# confirm TensorFlow sees the GPU
+from tensorflow.python.client import device_lib
+# assert 'GPU' in str(device_lib.list_local_devices())
+
+# confirm Keras sees the GPU
+from keras import backend
+# print(len(backend.tensorflow_backend._get_available_gpus()) > 0)
