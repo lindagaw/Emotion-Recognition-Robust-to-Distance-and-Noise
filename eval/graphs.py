6b4cc383946b3cbc832790c53147c724e0d4f60f
Ash Gao <gaoye0718@gmail.com>
2019-09-26 00:25:10 -0400

    Alas! Only our panic is to blame.

--- eval/graphs.py
+++ eval/graphs.py
@@ -3,23 +3,43 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-category_names = ['error rate', 'accuracy']
-results = {'Happy': [0.0030, 0.9970], 'Angry': [0.0031, 0.9969], 'Neutral': [0.0, 1.0], 'Sad': [0.0, 1.0]
-           }
-graph_title = 'my_graph_title'
-
-def draw_scatter(h, a, n, s, title):
-    happy_x, happy_y = h
-    angry_x, angry_y = a
-    neutral_x, neutral_y = n
-    sad_x, sad_y = s
+def sort_pair_ascending_xs(xs, ys):
+    li = []
+    for x, y in zip(xs, ys):
+        li.append((x, y))
     
-    sns.set(style="whitegrid")
-    sns.residplot(happy_x, happy_y, color="g").set_title(title)
-    sns.residplot(angry_x, angry_y, color="r").set_ylabel('score on the correct class')
-    sns.residplot(neutral_x, neutral_y, color="y").set_xlabel('deamplified amount measured in decibels')
-    sns.residplot(sad_x, sad_y, color="b")
+    li = sorted(li, key=lambda x: x[0])
+    
+    ret_xs = []
+    ret_ys = []
+    for x, y in li:
+        ret_xs.append(x)
+        ret_ys.append(y)
+        
+    return ret_xs, ret_ys
 
+def draw_scatter(h, a, n, s, title, xlabel, ylabel):
+    happy_x, happy_y = sort_pair_ascending_xs(h[0], h[1])
+    angry_x, angry_y = sort_pair_ascending_xs(a[0], a[1])
+    neutral_x, neutral_y = sort_pair_ascending_xs(n[0], n[1])
+    sad_x, sad_y = sort_pair_ascending_xs(s[0], s[1])
+
+    fig, ax = plt.subplots()
+    scale = 5
+    
+    ax.set_title(title, y=-0.4)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    
+    ax.scatter(happy_x, happy_y, c='green', s=scale, label='Happy', alpha=0.6, edgecolors='none')
+    ax.scatter(angry_x, angry_y, c='red', s=scale, label='Angry', alpha=0.6, edgecolors='none')
+    ax.scatter(neutral_x, neutral_y, c='yellow', s=scale, label='Neutral', alpha=0.6, edgecolors='none')
+    ax.scatter(sad_x, sad_y, c='blue', s=scale, label='Sad', alpha=0.6, edgecolors='none')
+    ax.legend()
+    ax.grid(True)
+    plt.show()
+    
+    
 def draw(results, category_names, graph_title):
     labels = list(results.keys())
     data = np.array(list(results.values()))
@@ -45,7 +65,7 @@ def draw(results, category_names, graph_title):
         for y, (x, c) in enumerate(zip(xcenters, widths)):
             if c < 0.01:
                 continue
-            ax.text(x, y, str(float(c)), ha='center',
+            ax.text(x, y, str(float(c))[:5], ha='center',
                     va='center', color=text_color)
     ax.legend(ncol=len(category_names), bbox_to_anchor=(0, 1),
               loc='lower left', fontsize='small')
