c61895703af53a600b979f1c0c71cbf3654fa727
Ash Gao <gaoye0718@gmail.com>
2019-09-27 14:13:52 -0400

    A distant planet is panicked that one day it may be habitable to humanity.

--- eval/graphs.py
+++ eval/graphs.py
@@ -3,12 +3,20 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-def histogram(h_acc, a_acc, n_acc, s_acc, x_measurement, x_description, title):
+def histogram(h, a, n, s, x_description, title):
+    
+    x_measurement = [2, 4, 6, 8]
+    
+    h_x, h_acc = sort_pair_ascending_xs(h[0], h[1])
+    a_x, a_acc = sort_pair_ascending_xs(a[0], a[1])
+    n_x, n_acc = sort_pair_ascending_xs(n[0], n[1])
+    s_x, s_acc = sort_pair_ascending_xs(s[0], s[1])
+    
     x = np.arange(len(x_measurement))  # the label locations
     width = 0.2  # the width of the bars
     fig, ax = plt.subplots()
     
-    rect1 = ax.bar(x - 2*width, h_acc, width, label='Accuracy on Happy', color='#8dff33')
+    rect1 = ax.bar(x - 2 * width, h_acc, width, label='Accuracy on Happy', color='#8dff33')
     rect2 = ax.bar(x - width, a_acc, width, label='Accuracy on Angry', color='#ff5733')
     rect3 = ax.bar(x, n_acc, width, label='Accuracy on Neutral', color='#ffd133')
     rect4 = ax.bar(x + width, s_acc, width, label='Accuracy on Sad', color='#3374ff')
@@ -43,16 +51,16 @@ def draw_scatter(h, a, n, s, title, xlabel, ylabel):
     sad_x, sad_y = sort_pair_ascending_xs(s[0], s[1])
 
     fig, ax = plt.subplots()
-    scale = 10
+    scale = 30
     
     ax.set_title(title, y=-0.4)
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
     
-    ax.scatter(happy_x, happy_y, c='#8dff33', s=scale, label='Happy', alpha=0.6, edgecolors='none')
-    ax.scatter(angry_x, angry_y, c='#ff5733', s=scale, label='Angry', alpha=0.6, edgecolors='none')
-    ax.scatter(neutral_x, neutral_y, c='#ffd133', s=scale, label='Neutral', alpha=0.6, edgecolors='none')
-    ax.scatter(sad_x, sad_y, c='#3374ff', s=scale, label='Sad', alpha=0.6, edgecolors='none')
+    ax.scatter(happy_x, happy_y, c='#8dff33', s=scale, label='Happy', edgecolors='none')
+    ax.scatter(angry_x, angry_y, c='#ff5733', s=scale, label='Angry', edgecolors='none')
+    ax.scatter(neutral_x, neutral_y, c='#ffd133', s=scale, label='Neutral', edgecolors='none')
+    ax.scatter(sad_x, sad_y, c='#3374ff', s=scale, label='Sad', edgecolors='none')
     ax.legend()
     ax.grid(True)
     plt.show()
